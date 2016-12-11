b9ce63ce7a078b3768a2dc88e6d7122db552f623
Michał Dolny <michal.dolny@getresponse.com>
2016-12-11 16:18:21 +0100

    js do nawigacji obrazkowej

--- scripts.js
+++ scripts.js
@@ -1,68 +1,73 @@
-var carouselList = $('#carousel > ul');
-var loading = $('#carousel > i');
-// var carouselNavList = $('.carousel-nav');
-
-
 $(function() {
+    var carouselList = $('.preview-box');
+    var carouselListItems = $('.preview-box li');
+    var loading = $('#carousel > i');
+    var carouselNavList = $('.carousel-nav');
+
 	setInterval(rollSlideRight, 6000);
 	var leftHand = $('.left-hand');
 	var rightHand = $('.right-hand');
-	leftHand.click(function() { 
+	leftHand.click(function() {
 		rollSlideLeft();
 	});
-	rightHand.click(function() { 	
+	rightHand.click(function() {
 		rollSlideRight();
 	});
+
+    function rollFirstSlide() {
+    	var firstItem = carouselList.find('li:first');
+    	var lastItem = carouselList.find('li:last');
+    	lastItem.after(firstItem);
+    	carouselList.css({marginLeft:-400});
+    	loading.css('display','none');
+    }
+
+    function rollSlideRight() {
+    	loading.css('display','initial');
+    	carouselList.animate(
+    		{'marginLeft': -800},
+    		500,
+    		rollFirstSlide
+    	);
+    //	updateNavigation();
+    }
+
+    function rollLastSlide() {
+    	var firstItem = carouselList.find('li:first');
+    	var lastItem = carouselList.find('li:last');
+    	firstItem.before(lastItem);
+    	carouselList.css({marginLeft:-400});
+    	loading.css('display','none');
+    }
+
+    function rollSlideLeft() {
+    	loading.css('display','initial');
+    	carouselList.animate(
+    		{'marginLeft': 0},
+    		500,
+    		rollLastSlide
+    	);
+    //	updateNavigation();
+    }
+
+    // function updateNavigation() {
+    //     $.each(carouselNavList, function(element) {
+    //         (element).append(carouselList);
+    //     });
+    // }
+
+    // var imageNum = [0, 1, 2, 3, 4, 5];
+
+    // function slideDots(imageNum) {
+
+    // }
+
+    function updateNavigation() {
+    	$.each(carouselListItems, function(index, element) {
+            $(element).clone().appendTo(carouselNavList);
+    	});
+    }
+    updateNavigation();
+
 });
 
-function rollFirstSlide() {
-	var firstItem = carouselList.find('li:first');
-	var lastItem = carouselList.find('li:last');
-	lastItem.after(firstItem);
-	carouselList.css({marginLeft:-400});
-	loading.css('display','none');
-}
-
-function rollSlideRight() {
-	loading.css('display','initial');
-	carouselList.animate(
-		{'marginLeft': -800},
-		500,
-		rollFirstSlide
-		);
-//	updateNavigation();
-}
-
-function rollLastSlide() {
-	var firstItem = carouselList.find('li:first');
-	var lastItem = carouselList.find('li:last');
-	firstItem.before(lastItem);
-	carouselList.css({marginLeft:-400});
-	loading.css('display','none');
-}
-
-function rollSlideLeft() {
-	loading.css('display','initial');
-	carouselList.animate(	
-		{'marginLeft': 0},
-		500,
-		rollLastSlide
-		);
-//	updateNavigation();
-}
-
-/*
-function updateNavigation() {
-	$.each(carouselNavList, function(element) {
-		(element).append(carouselList);
-	});
-}
-
-
-var imageNum = [0, 1, 2, 3, 4, 5];
-
-function slideDots(imageNum) {
-
-}
-
-*/
